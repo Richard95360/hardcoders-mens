760efe36532926ca75e8f453f9ddb1e1ea1288aa
Richard Haddad <richard.haddad-ext@bnf.fr>
2021-03-26 13:14:09 +0100

    first commit

---
+++ client/src/components/Rooms.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from "react";
+import RoomCard from './RoomCard'
+import {Link }from 'react-router-dom'
+
+const Rooms = () => {
+    const [rooms, setRooms] = useState([])
+
+ useEffect(() => {
+ const fetchData = async () => {
+     const data = await window.fetch('/api/rooms')
+     const json = await data.json()
+   
+     setRooms(json)
+ }
+
+ fetchData()
+ },[])
+
+    return (
+        <>
+        {rooms.map(room => (
+            <Link  key={room._id} to={room._id}>
+              <RoomCard room={room}></RoomCard>
+
+            </Link>
+        ))}
+
+        </>
+  )
+}
+
+export default Rooms
